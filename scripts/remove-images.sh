d7e5a1dce1c29ba0e5d538397f822f27e4cdc896
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2022-08-18 10:50:07 +0530

    Updated github linting check, added removal of docker containeers and local npm registry

--- scripts/remove-images.sh
+++ scripts/remove-images.sh
@@ -1,3 +1,8 @@
+# remove containers
+docker rm -f local_l1-api_1
+docker rm -f local_l1-mongo_1
+docker rm -f local_l2-api_1
+
 # remove the l1 docker images create previously
 docker image rm local_l1-api:latest
 docker image rm local_l2-api:latest
