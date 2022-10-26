fad6bd1eb3cbc987dfed0ad76e7c4128f383e419
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2022-10-26 19:23:32 +0530

    Fixed Overwrite Bug in Network Registry

--- registry/src/helpers/db/create.js
+++ registry/src/helpers/db/create.js
@@ -6,7 +6,10 @@ module.exports = async (id, object, schemaName) => {
 
         // Check if Object already exists or not
         const check = await Schema.find(id);
-        if (check.length > 0) throw new Error('Entry already exists! Create action aborted.');
+        if (check.length > 0) {
+            console.log('Entry already exists! Create action aborted.');
+            return 200;
+        }
 
         // Create the schema object into the database
         const doc = await Schema.create(object);
