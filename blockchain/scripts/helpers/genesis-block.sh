dbdf18370eaccdce3aac891da22d0b767405666c
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2022-10-28 21:22:09 +0530

    Fixed problems with Genesis Block Generation

--- blockchain/scripts/helpers/genesis-block.sh
+++ blockchain/scripts/helpers/genesis-block.sh
@@ -1,7 +1,16 @@
-cat ./config/configtx/local.1.yml ./config/configtx/local.2.yml ./config/configtx/state.1.yml ./config/configtx/national.yml ./config/configtx/common.yml >./gen/configtx.yml
+cat ./config/configtx/local.1.yml \
+    ./config/configtx/local.2.yml \
+    ./config/configtx/state.1.yml \
+    ./config/configtx/national.yml \
+    ./config/configtx/common.yml >./gen/configtx.yml
 
 cd ./gen
 
 mkdir system-genesis-block
 
-./bin/configtxgen -profile HealthChainOrdererGenesis -channelID system-channel -outputBlock ./system-genesis-block/genesis.block
+cd ..
+
+./gen/bin/configtxgen -configPath ./gen \
+    -profile HealthChainOrdererGenesis \
+    -channelID system-channel \
+    -outputBlock ./gen/system-genesis-block/genesis.block
