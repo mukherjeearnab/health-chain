ca966f255b4aeec1ac35317bbe074da61c824b2b
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2022-10-29 13:22:18 +0530

    Finally able to create channel. Yaygit status

--- blockchain/scripts/container/create-channel.sh
+++ blockchain/scripts/container/create-channel.sh
@@ -1,25 +1,31 @@
 CHANNEL_NAME="mainchannel"
 MAX_RETRY=10
-DELAY=10
-BLOCKFILE="/opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/${CHANNEL_NAME}.block"
-ORDERER_CA="/opt/gopath/src/github.com/hyperledger/fabric/peer/organizations/ordererOrganizations/blc.healthchain.com/orderers/orderer.blc.healthchain.com/msp/tlscacerts/tlsca.blc.healthchain.com-cert.pem"
+DELAY=20
 
-CORE_PEER_LOCALMSPID=NatioanlMSP
+BLOCKFILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/${CHANNEL_NAME}.block
+ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/organizations/ordererOrganizations/blc.healthchain.com/orderers/orderer.blc.healthchain.com/msp/tlscacerts/tlsca.blc.healthchain.com-cert.pem
+
+CORE_PEER_LOCALMSPID=NationalMSP
 CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/organizations/peerOrganizations/id1.national.healthchain.com/peers/peer0.id1.national.healthchain.com/tls/ca.crt
-CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/id1.national.healthchain.com/users/Admin@id1.national.healthchain.com/msp
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/organizations/peerOrganizations/id1.national.healthchain.com/users/Admin@id1.national.healthchain.com/msp
 CORE_PEER_ADDRESS=peer0.id1.national.healthchain.com:13132
-CHANNEL_NAME=mainchannel
+
 CORE_PEER_TLS_ENABLED=true
 ORDERER_SYSCHAN_ID=system-channel
 
-createChannel() {
-
-    # Poll in case the raft leader is not set yet
-    peer channel create -o orderer.blc.healthchain.com:7050 -c $CHANNEL_NAME --ordererTLSHostnameOverride orderer.blc.healthchain.com -f ./channel-artifacts/${CHANNEL_NAME}.tx --outputBlock $BLOCKFILE --tls --cafile $ORDERER_CA >&log.txt
-
-    cat log.txt
-}
-
 echo "Creating channel ${CHANNEL_NAME}"
-createChannel
+
+sleep $DELAY
+
+# Poll in case the raft leader is not set yet
+peer channel create -o orderer.blc.healthchain.com:7050 \
+    -c $CHANNEL_NAME \
+    --ordererTLSHostnameOverride orderer.blc.healthchain.com \
+    -f ./channel-artifacts/${CHANNEL_NAME}.tx \
+    \
+    --tls \
+    --cafile $ORDERER_CA >&log.txt # --outputBlock $BLOCKFILE \
+
+cat log.txt
+
 echo "Channel '$CHANNEL_NAME' created"
--- blockchain/scripts/run.sh
+++ blockchain/scripts/run.sh
@@ -2,4 +2,4 @@ echo "Orchestrating the Blockchain Network...."
 bash ./scripts/helpers/network-up.sh
 
 echo "Creating Communication Channel for Org Peers"
-# bash ./scripts/helpers/create-channel.sh
+bash ./scripts/helpers/create-channel.sh
