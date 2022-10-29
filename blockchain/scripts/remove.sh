7d934ab11ef4117d417f247e78f48e98d23f86bc
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2022-10-29 20:01:23 +0530

    Added Commands to remove volumes and images

--- blockchain/scripts/remove.sh
+++ blockchain/scripts/remove.sh
@@ -10,4 +10,9 @@ docker rm -f healthchain_ca.id1.state.healthchain.com_1
 docker rm -f healthchain_peer0.id1.state.healthchain.com_1
 docker rm -f healthchain_ca.id2.local.healthchain.com_1
 
-docker volume prune
+# remove docker volumes
+docker volume ls --format '{{.Name}}' | grep healthchain.com | xargs docker volume rm -f
+
+# remove the chaincode containers and images
+docker ps -a --format '{{.Names}}' | grep dev-peer | xargs docker rm -f
+docker images --format '{{.Repository}}' | grep dev-peer | xargs docker image rm -f
