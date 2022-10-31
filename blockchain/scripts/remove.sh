ac0e057cd2442d8c9468054a78629782ac9c992e
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2022-10-31 17:02:42 +0530

    Resolved Bug related to removal of volumes

--- blockchain/scripts/remove.sh
+++ blockchain/scripts/remove.sh
@@ -1,4 +1,4 @@
-echo "Removing all Containers of Healthchain Blockchain"
+echo "Removing all Containers and Volumes of Healthchain Blockchain"
 docker ps -a --format '{{.Names}}' | grep healthchain_ca | xargs docker rm -f
 docker ps -a --format '{{.Names}}' | grep healthchain_peer0 | xargs docker rm -f
 docker ps -a --format '{{.Names}}' | grep healthchain_orderer | xargs docker rm -f
@@ -6,11 +6,13 @@ docker ps -a --format '{{.Names}}' | grep healthchain_cli | xargs docker rm -f
 docker ps -a --format '{{.Names}}' | grep healthchain_api.blc | xargs docker rm -f
 docker ps -a --format '{{.Names}}' | grep dev-peer | xargs docker rm -f
 
+docker volume ls --format '{{.Name}}' | grep healthchain.com | xargs docker volume rm -f
+
 # remove the docker images create previously
 if [[ "$1" = "-c" ]]; then
-    echo "Removing Images, Volumes and Networks as well...."
-    # remove docker volumes
-    docker volume ls --format '{{.Name}}' | grep healthchain.com | xargs docker volume rm -f
+    echo "Removing Images as well...."
+    # # remove docker volumes
+    # docker volume ls --format '{{.Name}}' | grep healthchain.com | xargs docker volume rm -f
 
     # remove the chaincode images
     docker images --format '{{.Repository}}' | grep dev-peer | xargs docker image rm -f
