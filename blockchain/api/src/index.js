d823431f389202a813a29053829d712f32dc9f1d
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2025-01-06 16:24:20 +0530

    Updated CORS for blockchain API

--- blockchain/api/src/index.js
+++ blockchain/api/src/index.js
@@ -1,6 +1,6 @@
 // import packages
 const express = require('express');
-// const cors = require("cors");
+const cors = require('cors');
 
 // initialize express app
 const app = express();
@@ -12,13 +12,13 @@ require('dotenv').config();
 app.use(express.json());
 
 // CORS Middleware
-// app.use(cors());
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-//     res.header("Access-Control-Allow-Headers", "Content-Type");
-//     next();
-// });
+app.use(cors());
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 // Load API Routes
 require('./routes')(app);
